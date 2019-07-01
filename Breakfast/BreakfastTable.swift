16e165d81bdf74e729b9b670ddbdd48f0a5f9ebb
Steve Lipton <steve@makeapppie.com>
2019-07-01 05:15:04 -0500

    Initial Commit

---
+++ Breakfast/BreakfastTable.swift
@@ -0,0 +1,32 @@
+//
+//  BreakfastTable.swift
+//  Breakfast
+//
+//
+//  An exercise file for iOS Development Tips Weekly
+//  by Steven Lipton (C)2018, All rights reserved
+//  For videos go to http://bit.ly/TipsLinkedInLearning
+//  For code go to http://bit.ly/AppPieGithub
+//
+//  Created by Steven Lipton on 6/29/19.
+//  Copyright © 2019 Steven Lipton. All rights reserved.
+//
+
+import SwiftUI
+
+struct BreakfastTable : View {
+    var breakfastModel:[Breakfast]{
+        return BreakFastModel().breakfast
+    }
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct BreakfastTable_Previews : PreviewProvider {
+    static var previews: some View {
+        BreakfastTable()
+    }
+}
+#endif
